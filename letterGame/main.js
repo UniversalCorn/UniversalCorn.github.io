46c221f052e3df809e4d11540d4173ae18577e1a
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-04-14 18:33:15 +0300

    Update main.js

--- letterGame/main.js
+++ letterGame/main.js
@@ -9,46 +9,37 @@ let randVertBack = backVert[rand];
 
 let container = document.getElementById('container');
 
+let headliner = document.getElementById('headliner');
+headliner.style.background = 'rgba(256, 64, 64, 0.7)';
+setTimeout(function() {
+	headliner.style.display = 'inline-block';
+}, 1);
+headliner.style.left = -headliner.offsetWidth + 'px';
 setInterval(function() {
+	headliner.style.top = window.innerHeight/2 - headliner.offsetHeight/2 + 'px';
 	if (window.innerWidth < window.innerHeight) {
+		headliner.style.width = '60%';
 		container.style.backgroundImage = `url(${randVertBack})`;
 	}
 	else {
+		headliner.style.width = '40%';
 		container.style.backgroundImage = `url(${randBack})`;
 	}
-}, 0)
-
-let pic1 = document.getElementsByTagName('img')[0];
-let pic2 = document.getElementsByTagName('img')[1];
-let pic3 = document.getElementsByTagName('img')[2];
-
-let headliner = document.getElementById('headliner');
-headliner.style.left = -headliner.offsetWidth + 'px';
-headliner.style.top = window.innerHeight/2 - headliner.offsetHeight/2 + 'px';
-headliner.style.height = '40%';
-
-
-setTimeout( function() {
-		moveInner();	
-}, 2000 + (window.innerWidth/2 + headliner.offsetWidth/2) /2);
+}, 0);
 
 function moveOuter() {
 	let start = Date.now()
 	let timer = setInterval(function() {
 		let timePassed = Date.now() - start;	
 		if ( (Number((headliner.style.left).slice(0, -2))) > ( (window.innerWidth/2) - (headliner.offsetWidth/2))  ) {
+			setTimeout( function() {
+				moveInner();
+			}, 1500);
 			clearInterval(timer);
 			return;
 		}
 		draw(timePassed);
 	}, 10);
-	setInterval(function() {
-		if (window.innerWidth < window.innerHeight) {
-			headliner.style.width = '60%';
-			headliner.style.background = 'rgba(256, 64, 64, 0.7)';
-			document.body.style.fontSize = '1 rem';
-		}
-	}, 0);
 }
 
 moveOuter();
@@ -71,7 +62,7 @@ function draw(timePassed) {
 }
 
 function drawInner(timePassed) {
-	headliner.style.left = (timePassed * 2)  + window.innerWidth - headliner.offsetWidth*1.75 + 'px';
+	headliner.style.left = (timePassed * 2)  + window.innerWidth/2 - headliner.offsetWidth/2 + 'px';
 }
 
 
@@ -88,7 +79,6 @@ setInterval(function() {
 			document.getElementsByTagName('img')[i].style.transform = 'scale(2)';
 		}
 	}
-
 	else {
 		for (let i = 0; i < 3; i++) {
 			document.getElementsByTagName('img')[i].style.transform = 'scale(1.2)';
@@ -96,33 +86,12 @@ setInterval(function() {
 	}
 }, 0);
 
-function mouseOver (target) {
-	return function() {
-		target.style.cursor = 'pointer';
-	}
-}
-
-function mouseOut (target) {
-	return function() {
-		target.style.cursor = 'default';
-	}
-}
-
 function mouseDown (target, strLink) {
 	return function() {
 		window.location.href = strLink;
 	}
 }
 
-pic1.onmouseover = mouseOver(pic1);
-pic1.onmouseout = mouseOut(pic1);
-pic1.onmousedown = mouseDown(pic1, links[0]);
-
-pic2.onmouseover = mouseOver(pic2);
-pic2.onmouseout = mouseOut(pic2);
-pic2.onmousedown = mouseDown(pic2, links[1]);
-
-pic3.onmouseover = mouseOver(pic3);
-pic3.onmouseout = mouseOut(pic3);
-pic3.onmousedown = mouseDown(pic3, links[2]);
-
+for (let i = 0; i < 3; i++){
+document.getElementsByTagName('img')[i].onmousedown = mouseDown(document.getElementsByTagName('img')[i], links[i])
+}
