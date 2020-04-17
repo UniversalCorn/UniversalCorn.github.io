784ef2b76e683cf13029c1bb47fd9aa6e8ba14cd
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-04-17 21:58:56 +0300

    Update test.js

--- letterGame/test.js
+++ letterGame/test.js
@@ -13,6 +13,9 @@ const relation = [ ['Аа', 'a.mp3'], ['Бб', 'b.mp3'], ['Вв', 'v.mp3'], ['Г
  let right = 0;
  let wrong = 0;
  let picsAmount = 33;
+ let currentSound = new Audio();
+ currentSound.preload = 'auto';
+ currentSound.src = '';
 
  let back = ['b1.jpg', 'b2.jpg', 'b3.jpg', 'b4.jpg'];
  let backVert = ['b1vert.jpg', 'b2vert.jpg', 'b3vert.jpg', 'b4vert.jpg'];
@@ -21,6 +24,20 @@ const relation = [ ['Аа', 'a.mp3'], ['Бб', 'b.mp3'], ['Вв', 'v.mp3'], ['Г
 let rand = Math.floor(Math.random() * back.length);
 let randBack = back[rand];
 let randVertBack = backVert[rand];
+
+let menuColor = '';
+if (rand === 0) {
+	menuColor = 'rgba(18, 42, 256, 0.8)';
+}
+else if (rand === 1) {
+	menuColor = 'rgba(256, 122, 138, 0.8)';
+}
+else if (rand === 2) {
+	menuColor = 'rgba(34, 136, 256, 0.8)';
+}
+else {
+	menuColor = 'rgba(82, 96, 256, 0.8)';
+}
 
 
 setInterval(function() {
@@ -48,27 +65,213 @@ setInterval(function() {
 			document.getElementsByClassName('letter')[i].style.fontSize = '5vw';
 		}
 	}
-}, 1)
-
-let audioStart = new Audio();
-audioStart.preload = 'auto';
-audioStart.src = 'выбериM.mp3';
+}, 1);
 
 function playButton() {
 	return function(){
+		menu.style.display = 'flex';
 		playB.style.display = 'none';
-		setTimeout(function() {
-			audioStart.play();
-			}, 500);
+		document.body.style.justifyContent = 'center';
+		audioStart.play();
 		setTimeout(function() {
 			Question();
 			for (let i = 0; i < 33; i++) {
 				document.getElementsByClassName('letter')[i].style.display = 'inline-block';
+				document.getElementById('listen').style.display = 'inline-block';
+				document.getElementById('settings').style.display = 'inline-block';
 			}
-		}, 3500);
+		}, 2250);
 	}
 }
 
+const playB = document.getElementsByTagName('img')[0];
+playB.onmousedown = playButton();
+
+function MoveLeft() {
+	let start = Date.now();
+	let timer = setInterval(function() {
+		let timePassed = Date.now() - start;
+		if ( Number(menu.style.left.slice(0,-2)) <= 0) {
+			clearInterval(timer);
+
+			return;
+		}
+		DrawLeft(timePassed);
+	}, 0);
+}
+
+function MoveRight() {
+	let start = Date.now();
+	let timer = setInterval(function() {
+		let timePassed = Date.now() - start;
+		if ( Number(menu.style.left.slice(0,-2)) >= window.innerWidth) {
+			menu.style.left = '100vw'
+			clearInterval(timer);
+			return;
+		}
+		DrawRight(timePassed);
+	}, 0);
+}
+
+function DrawLeft(timePassed) {
+	menu.style.left = -(timePassed*4 - menu.offsetWidth) + 'px';
+}
+
+function DrawRight(timePassed) {
+	menu.style.left = timePassed*4 + 'px';
+}
+
+let listen = document.getElementById('listen');
+listen.style.position = 'fixed';
+listen.style.left = '0.5vw';
+listen.style.bottom = '0.5vw';
+listen.addEventListener('click', function() {
+	currentSound.play();
+});
+
+let settings = document.getElementById('settings');
+settings.style.position = 'fixed';
+settings.style.right = '0.5vw';
+settings.style.bottom = '0.5vw';
+settings.addEventListener('click', function() {
+	settings.style.display = 'none';
+	MoveLeft();
+});
+
+let menu = document.getElementById('menu');
+menu.style.background = menuColor;
+let pageClose = document.getElementById('pageClose');
+pageClose.addEventListener('click', function() {
+	window.location.href = 'main.html';
+});
+let menuRepeat = document.getElementById('menuRepeat');
+	menuRepeat.addEventListener('click', function() {
+	window.location.reload(false);	
+});
+let menuArrow = document.getElementById('menuArrow');
+menuArrow.addEventListener('click', function() {
+	settings.style.display = 'inline-block';
+	MoveRight();
+});
+
+menu.style.left = '100vw';
+menu.style.width = '105vw';
+menu.style.height = '20vw';
+for (let i = 0; i < 3; i++) {
+	document.getElementsByClassName('menuImage')[i].style.height = '60%';
+	document.getElementsByClassName('menuImage')[i].style.width = document.getElementsByClassName('menuImage')[i].style.height + 'px';
+	document.getElementsByClassName('menuImage')[i].style.margin =  '0 6vw';
+}
+
+let staticWidth = '';
+if (window.innerWidth < window.innerHeight/2) {
+	let minWidth = setInterval( function() {
+		if (window.innerWidth < window.innerHeight/2) {
+			listen.style.width = '15.384615%';
+			staticWidth = window.innerWidth * 0.15384615;
+			listen.style.height = window.innerWidth/6.5 + 'px';
+			settings.style.width = '15.384615%';
+			settings.style.height = window.innerWidth/6.5 + 'px';
+		}
+		else if ((window.innerWidth >= window.innerHeight/2) && (window.innerWidth < window.innerHeight*2)) {
+			listen.style.width = staticWidth + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth + 'px';
+			settings.style.height = listen.style.width;
+		}
+		else {
+			listen.style.width = staticWidth*1.35 + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth*1.35 + 'px';
+			settings.style.height = listen.style.width;
+		}
+	}, 0);
+}
+else if ((window.innerWidth >= window.innerHeight/2) && (window.innerWidth < window.innerHeight)) {
+	let midWidth = setInterval( function() {
+		if (window.innerWidth < window.innerHeight/2) {
+			listen.style.width = '15.384615%';
+			listen.style.height = window.innerWidth/6.5 + 'px';
+			settings.style.width = '15.384615%';
+			settings.style.height = window.innerWidth/6.5 + 'px';
+		}
+		else if ((window.innerWidth >= window.innerHeight/2) && (window.innerWidth < window.innerHeight)) {
+			staticWidth = window.innerWidth/2 * 0.15384615;
+			listen.style.width = staticWidth + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth + 'px';
+			settings.style.height = listen.style.width;
+		}
+		else if ((window.innerWidth >= window.innerHeight) && (window.innerWidth < window.innerHeight*2)) {
+			listen.style.width = staticWidth/1.2 + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth/1.2 + 'px';
+			settings.style.height = listen.style.width;
+		}
+		else {
+			listen.style.width = staticWidth*1.2 + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth*1.2 + 'px';
+			settings.style.height = listen.style.width;
+		}
+	}, 0);
+}
+else if ((window.innerWidth >= window.innerHeight) && (window.innerWidth < window.innerHeight*2)) {
+	let midWidth2 = setInterval( function() {
+		if (window.innerWidth < window.innerHeight/2) {
+			listen.style.width = '15.384615%';
+			listen.style.height = window.innerWidth/6.5 + 'px';
+			settings.style.width = '15.384615%';
+			settings.style.height = window.innerWidth/6.5 + 'px';
+		}
+		else if ((window.innerWidth >= window.innerHeight/2) && (window.innerWidth < window.innerHeight)) {
+			listen.style.width = staticWidth + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth + 'px';
+			settings.style.height = listen.style.width;
+		}
+		else if ((window.innerWidth >= window.innerHeight) && (window.innerWidth < window.innerHeight*2)) {
+			staticWidth = window.innerWidth/2 * 0.15384615;
+			listen.style.width = staticWidth/1.2 + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth/1.2 + 'px';
+			settings.style.height = listen.style.width;
+		}
+		else {
+			listen.style.width = staticWidth*1.2 + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth*1.2 + 'px';
+			settings.style.height = listen.style.width;
+		}
+	}, 0);
+}
+else {
+	let maxWidth = setInterval( function() {
+		if (window.innerWidth < window.innerHeight/2) {
+			listen.style.width = '15.384615%';
+			listen.style.height = window.innerWidth/6.5 + 'px';
+			settings.style.width = '15.384615%';
+			settings.style.height = window.innerWidth/6.5 + 'px';
+		}
+		else if ((window.innerWidth >= window.innerHeight/2) && (window.innerWidth < window.innerHeight*2)) {	
+			listen.style.width = staticWidth + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth + 'px';
+			settings.style.height = listen.style.width;
+		}
+		else {
+			staticWidth = window.innerWidth/4 * 0.15384615;
+			listen.style.width = staticWidth*1.5 + 'px';
+			listen.style.height = listen.style.width;
+			settings.style.width = staticWidth*1.5 + 'px';
+			settings.style.height = listen.style.width;
+		}
+	}, 0);
+}
+
+let audioStart = new Audio();
+audioStart.preload = 'auto';
+audioStart.src = 'выбери' + 'M.mp3';
 
 let letterSound = new Audio();
 audioStart.preload = 'auto';
@@ -79,7 +282,10 @@ let rightSum = 0;
 let wrongSum = 0;
 function Question() {
 	if (picsAmount === 0) { 
-		rememberMas = rememberMas.map((val, i) => (i!=0 && val==rememberMas[i-1]) ? 0 : 1 );
+		let audioFinish = new Audio();
+		audioFinish.preload = 'auto';
+		audioFinish.src = 'molodec' + 'M.mp3';
+		rememberMas = rememberMas.map((val, i) => (i!=0 && val == rememberMas[i-1]) ? 0 : 1 );
 		wrongSum = rememberMas.reduce((acc, val) => acc + val, 0)
 		rightSum = right;
 		let points = rightSum - wrongSum + 1;
@@ -120,6 +326,7 @@ function Question() {
 			}
 			else {
 				panel.innerText += ` Молодец!`
+				audioFinish.play();
 				panel.style.background = 'rgba(50, 250, 50, 0.5)';
 			}
 			let br = document.createElement('br');
@@ -145,6 +352,7 @@ function Question() {
 		for (let i = 0; i < sounds.length; i++) {
 			if (fileName === sounds[i].slice(0,-4) + 'M.mp3') {
 				rememberSound = sounds[i];
+				currentSound.src = sounds[i].slice(0,-4) + 'M.mp3';
 				if (relation[i][1] === rememberSound) {
 					rememberSound = relation[i][0];
 				}
@@ -182,7 +390,3 @@ function Question() {
 		rememberMas.push(wrong);
 	}
 }
-
-
-const playB = document.getElementsByTagName('img')[0];
-playB.onmousedown = playButton();
