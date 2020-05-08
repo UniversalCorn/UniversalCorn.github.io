d3cea15ca0fbdcd81106b9addca3a6394503ae3d
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-05-08 23:39:11 +0300

    Update test.js

--- letterGame/test.js
+++ letterGame/test.js
@@ -162,7 +162,7 @@ let menu = document.getElementById('menu');
 menu.style.background = menuColor;
 let pageClose = document.getElementById('pageClose');
 pageClose.addEventListener('click', function() {
-	window.history.go(-1);
+	window.location.reload(true);
 	//window.history.pushState({ path: 'https://universalcorn.github.io/letterGame/main.html' }, '', 'https://universalcorn.github.io/letterGame/main.html');
 	//window.location.href = 'https://universalcorn.github.io/letterGame/main.html';
 	//location.reload();
