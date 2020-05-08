dd909e53e8c437860db82cbb88c1714d60bb8160
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-05-09 01:56:17 +0300

    Update guess.js

--- letterGame/guess.js
+++ letterGame/guess.js
@@ -164,8 +164,6 @@ function mouseClick(image) {
 	closeImage.style.top = '2%';
 	closeImage.onmouseover = mouseOver(closeImage);
 	closeImage.addEventListener('click', function(){
-		$(document).height() = 'auto';
-		conitainer.style.height = 'auto';
 		container.style.flexDirection = 'row';
 		container.style.justifyContent = 'center';
 		big.style.display = 'none';
@@ -207,7 +205,5 @@ let apelsin = document.querySelectorAll('img')[0];
 for (let i = 0; i < imageAmount; i++) {
 	document.querySelectorAll('img')[i].onclick = function() {
 		mouseClick(document.querySelectorAll('img')[i]);
-		$(document).height() = '100vh';
-		conitainer.style.height = '100vh';
 	}
 }
