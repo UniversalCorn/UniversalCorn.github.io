6b6c0e79cd45ba5f966054b7bffab307df8fda97
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-05-08 23:30:39 +0300

    Update test.js

--- letterGame/test.js
+++ letterGame/test.js
@@ -169,7 +169,7 @@ pageClose.addEventListener('click', function() {
 });
 let menuRepeat = document.getElementById('menuRepeat');
 	menuRepeat.addEventListener('click', function() {
-		window.location.href = window.location.href;
+		window.history.go(0);
 		//window.history.replaceState({ path: window.location.href }, '', window.location.href);
 });
 let menuArrow = document.getElementById('menuArrow');
