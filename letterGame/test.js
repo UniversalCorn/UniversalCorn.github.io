0839dc76df81e16c06f041636d9918c8d8080306
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-06-08 16:32:43 +0300

    Update test.js

--- letterGame/test.js
+++ letterGame/test.js
@@ -22,12 +22,7 @@ const currentSound = new Audio();
 currentSound.preload = 'auto';
 currentSound.src = '';
 
-const back = ['b1.jpg', 'b2.jpg', 'b3.jpg', 'b4.jpg'];
-const backVert = ['b1vert.jpg', 'b2vert.jpg', 'b3vert.jpg', 'b4vert.jpg'];
-
-const rand = Math.floor(Math.random() * back.length);
-const randBack = back[rand];
-const randVertBack = backVert[rand];
+import {back, backVert, rand, randBack, randVertBack} from './setBackAndRand.js';
 
 let menuColor = '';
 if (rand === 0) {
