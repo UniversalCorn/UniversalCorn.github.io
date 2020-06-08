be856108a0caa4a6b085a0567965ba5836442eff
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-06-08 13:28:37 +0300

    Update listen.js

--- letterGame/listen.js
+++ letterGame/listen.js
@@ -60,23 +60,6 @@ audioStart.src = 'нажми' + genderStr;
 const playB = document.getElementById('play');
 playB.onmousedown = playButton();
 
-let obj = {};
-for (let i = 0; i < picsAmount; i++) {
-  obj = document.createElement('div');
-  if (i < 6) {
-    obj.innerText = String.fromCharCode(i + 1040) +
-	String.fromCharCode(i + 1072);
-  } else if (i > 6) {
-    obj.innerText = String.fromCharCode(i + 1039) +
-	String.fromCharCode(i + 1071);
-  } else {
-    obj.innerText = String.fromCharCode(1025) +
-	String.fromCharCode(1105);
-  }
-  document.body.appendChild(obj);
-  obj.setAttribute('class', 'letter');
-}
-
 function playButton() {
   return function() {
     for (let i = 0; i <= picsAmount; i++) {
