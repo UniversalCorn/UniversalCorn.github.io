cfd8094e81104095ced63b21416850c62b5cecb5
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-06-08 16:08:39 +0300

    Update setGender.js

--- letterGame/setGender.js
+++ letterGame/setGender.js
@@ -1,6 +1,6 @@
 'use strict';
 export let genderStr = '';
-export function defineGender() {
+export function setGender() {
   const urlIfMale =
   'https://universalcorn.github.io/letterGame/main.html?gender=Male';
   genderStr = (document.referrer === urlIfMale) ? 'M.mp3' : 'F.mp3';
