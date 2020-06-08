7e4cd2c8462ff8a900fa323fdb8c642b8f29c308
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-06-08 15:49:12 +0300

    Delete defineGender.js

--- letterGame/defineGender.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-export let genderStr = '';
-export function defineGender() {
-  const urlIfMale =
-  'https://universalcorn.github.io/letterGame/main.html?gender=Male';
-  genderStr = (document.referrer === urlIfMale) ? 'M.mp3' : 'F.mp3';
-}
