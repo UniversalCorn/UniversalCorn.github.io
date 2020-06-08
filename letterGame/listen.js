dd1c810c87c7bc37227d3d62fff86cfc8e8fa970
UniversalCorn <37745544+UniversalCorn@users.noreply.github.com>
2020-06-08 11:16:17 +0300

    Update listen.js

--- letterGame/listen.js
+++ letterGame/listen.js
@@ -65,11 +65,14 @@ let obj = {};
 for (let i = 0; i < picsAmount; i++) {
       obj = document.createElement('div');
       if (i < 6) {
-      	obj.innerText = String.fromCharCode(i + 1072);
+      	obj.innerText = String.fromCharCode(i + 1040) +
+	String.fromCharCode(i + 1072);
       } else if (i > 6) {
-	obj.innerText = String.fromCharCode(i + 1071);
+	obj.innerText = String.fromCharCode(i + 1039) +
+	String.fromCharCode(i + 1071);
       } else {
-	obj.innerText = String.fromCharCode(1105);
+	obj.innerText = String.fromCharCode(1071)+
+	String.fromCharCode(1105);
       }
       document.body.appendChild(obj);
       obj.setAttribute('class', 'letter');
